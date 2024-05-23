50b81ccc94742b27ceb002f8f453bf54d8fbdffd
MrRobot-Less <gustavodev.dev@gmail.com>
2024-05-23 01:48:02 +0100

    chore: prep package-lock removing cache build

--- build/middlewares/auth-required.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function authRequiredMiddleware(req, res, next) {
-    if (req.user) {
-        return next();
-    }
-    res.redirect(req.originalUrl);
-}
-exports.default = authRequiredMiddleware;
--- build/translations/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ptBR = require('./pt-br.json');
-exports.default = {
-    ptBR
-};
